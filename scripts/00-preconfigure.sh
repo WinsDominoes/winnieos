36b1ef025d73076ee4685a3b68afebf0e67ac9ca
AtiusAmy <torv.device027@passmail.net>
2026-01-03 18:33:32 +0700

    Remove

--- scripts/00-preconfigure.sh
+++ scripts/00-preconfigure.sh
@@ -4,3 +4,7 @@ set -ouex pipefail
 
 # Apply IP Forwarding before installing Docker to prevent messing with LXC networking
 sysctl -p
+rm -rf /usr/share/plymouth
+rm -rf /usr/share/pixmaps
+rm -rf usr/share/icons/hicolor/scalable/places
+
