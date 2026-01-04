a9c9edcf81337210b5aed30d9dae49382fac44e8
Amy/Atius <136548767+AtiusAmy@users.noreply.github.com>
2026-01-04 16:15:44 +0700

    Update 07-cleanup.sh

--- scripts/07-cleanup.sh
+++ scripts/07-cleanup.sh
@@ -8,10 +8,6 @@ rm -vr /scripts
 mkdir -p /etc/flatpak/remotes.d/
 curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# GO AWAY fedora flatpaks.
-rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
-systemctl enable flatpak-add-flathub-repos.service
-
 # Saves a space
 rm -rf /usr/share/doc
 rm -rf /usr/bin/chsh # footgun
